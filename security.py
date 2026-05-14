43fde809697bc986f1ba079f9e0a8a79f43ecd07
oltamorina24 <olta.morina@student.uni-pr.edu>
2026-05-14 23:18:49 +0200

    changes

--- security.py
+++ security.py
@@ -8,23 +8,22 @@ cipher_suite = Fernet(ENCRYPTION_KEY)
 def generate_totp(username):
     if username not in users:
         raise Exception("User does not exist in the system!")
-  secret = users[username]
-  totp = pyotp.TOTP(secret)
-  
+    secret = users[username]
+    totp = pyotp.TOTP(secret)
     return totp.now()
 
 
 
 def verify_totp(username, code):
-   if username not in users:
+     if username not in users:
         return False
     
      secret = users[username]
      totp = pyotp.TOTP(secret)
      return totp.verify(code, valid_window=1)
 
-  def encrypt_payload(message):
+def encrypt_payload(message):
     return cipher_suite.encrypt(message.encode())
 
-  def decrypt_payload(encrypted_message):
+def decrypt_payload(encrypted_message):
     return cipher_suite.decrypt(encrypted_message).decode()
